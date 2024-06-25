973fe0ff66470105a29c9f4d5df9bfe259240b6d
Muhdal-Amin <adalemo2000@gmail.com>
2024-06-25 18:06:57 +0100

    Implementing conversation retention features using langchain's ConversationalRetrievalChain and ConversationBufferMemory to enable the AI answer follow up questions and have chat history stored in memory.

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceInstructEmbeddings
 #from InstructorEmbedding import INSTRUCTOR
 from langchain_community.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.llms import ChatOpenAI
 
 #function Prototypes
 
@@ -29,12 +32,24 @@ def get_text_chunks(text):
     chunks = text_splitter.split_text(text)
     return chunks
 
-# Function to create vector stores for text chunks
+# Function to create vector stores for embedded text chunks
 def get_vector_store(text_chunks):
     #embeddings = OpenAIEmbeddings()
     embeddings = HuggingFaceInstructEmbeddings(model_name = 'hkunlp/instructor-large')
     vectorstore = FAISS.from_texts(texts = text_chunks, embedding = embeddings)
     return vectorstore
+
+# Function to create a conversation change
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(memory_keys = 'chat_history', return_message=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm = llm,
+        retriever = vectorstore.as_retriever(),
+        memory = memory
+    )
+    return conversation_chain
+
     
 # main - Code entry point
 def main():
@@ -43,6 +58,12 @@ def main():
  
     # Set Page Configs.
     st.set_page_config(page_title="ASIRI AI", page_icon=":sparkles:")
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
 
     st.header("Welcome To ASIRI AI :sparkles:")
     st.markdown("***")
@@ -55,7 +76,7 @@ def main():
         st.subheader("Your Documents")
         pdf_docs = st.file_uploader("Upload your PDF files here", accept_multiple_files=True )
         if st.button("Run"):
-            with st.spinner("Generating...."):
+            with st.spinner("Processing..."):
                 
                 # Get PDF Document text
                 raw_text = get_pdf_text(pdf_docs)
@@ -65,6 +86,9 @@ def main():
 
                 # Creating Vector Store for text chunks
                 vector_store = get_vector_store(text_chunks)
+
+                # Creating a conversation chain
+                st.session_state.conversation = get_conversation_chain(vector_store)
                
             
     
